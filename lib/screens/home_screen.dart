28971af5c437eaa6877396342f28adc27aa628dd
Pom94 <pamelavictoria-11@hotmail.com>
2023-12-16 02:31:32 -0300

    Home screen and MenuOptionWidget changes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,92 +1,83 @@
+import 'package:flutter/material.dart';
 import 'package:drinks_and_meals/screens/meals_list.dart';
 import 'package:drinks_and_meals/screens/drinks.dart';
 import 'package:drinks_and_meals/widgets/MenuOptionWidget.dart';
 import 'package:drinks_and_meals/widgets/DrawerMenu.dart';
 import 'package:drinks_and_meals/widgets/CustomAppBar.dart';
-import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const HomeScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(title: 'Drinks&Meals'),
       drawer: DrawerMenu(),
-      body: ListView(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          FadeInImage(
-            placeholder: AssetImage('assets/images/loading.gif'),
-            image: NetworkImage(
-                'https://www.mccormick.com/-/media/project/oneweb/mccormick-us/mccormick/articles/info-graphic.jpg?rev=9300eb3510ca46e6af3abbf13b960985&vd=20210901T181349Z&hash=4F1DBA9346469A176C9A53F2F6ADD9CE'),
+          Container(
             height: 200,
-            fit: BoxFit.fitWidth,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                  'https://www.mccormick.com/-/media/project/oneweb/mccormick-us/mccormick/articles/info-graphic.jpg?rev=9300eb3510ca46e6af3abbf13b960985&vd=20210901T181349Z&hash=4F1DBA9346469A176C9A53F2F6ADD9CE',
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
+          SizedBox(height: 16),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               MenuOptionWidget(
                 text: 'Drinks',
                 imageUrl:
-                    'https://images.immediate.co.uk/production/volatile/sites/30/2021/11/GoodFood_Mocktails-8e3c536.jpg?quality=90&webp=true&fit=1300,866',
-                onTapAction: () => {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => DrinksScreen(),
-                    ),
-                  )
-                },
+                    'https://www.thecocktaildb.com/images/media/drink/fwpd0v1614006733.jpg',
+                onTapAction: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DrinksScreen(),
+                  ),
+                ),
               ),
               MenuOptionWidget(
                 text: 'Meals',
                 imageUrl:
-                    'https://www.mccormick.com/-/media/project/oneweb/mccormick-us/mccormick/articles/info-graphic.jpg?rev=9300eb3510ca46e6af3abbf13b960985&vd=20210901T181349Z&hash=4F1DBA9346469A176C9A53F2F6ADD9CE',
-                onTapAction: () => {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => MealsList(),
-                    ),
-                  )
-                },
+                    'https://www.themealdb.com/images/media/meals/vvstvq1487342592.jpg',
+                onTapAction: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MealsList(),
+                  ),
+                ),
               ),
             ],
           ),
-          Container(
-            padding: EdgeInsets.all(8.0),
+          SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
             child: Text(
               'Just one small positive thought in the morning can change your whole day',
               style: TextStyle(
-                fontSize: 24.0,
-                fontStyle: FontStyle.italic,
+                fontSize: 18.0,
+                fontStyle: FontStyle.italic, // Aplica cursiva al texto
                 color: Colors.black,
+                shadows: [
+                  Shadow(
+                    offset: Offset(0.0, 1.0),
+                    blurRadius: 3.0,
+                    color: Colors.black
+                        .withOpacity(0.5), // Ajusta la opacidad según prefieras
+                  ),
+                ],
               ),
+              textAlign: TextAlign.center,
             ),
-          )
+          ),
         ],
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {
-      //     print('hice click en el boton');
-      //   },
-      //   child: const Icon(
-      //     Icons.question_mark,
-      //     size: 30,
-      //   ),
-      // ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
-
-  // @override
-  // State<HomeScreen> createState() => _MyHomePageState();
 }
--- lib/widgets/MenuOptionWidget.dart
+++ lib/widgets/MenuOptionWidget.dart
@@ -15,30 +15,53 @@ class MenuOptionWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => onTapAction(),
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Image.network(
-            imageUrl,
-            fit: BoxFit.fill,
-            width: 200,
-            height: 200,
-          ),
-          Text(
-            text,
-            style: TextStyle(
-              fontSize: 24.0,
-              color: Colors.white,
-              shadows: [
-                Shadow(
-                  offset: Offset(0.0, 1.0),
-                  blurRadius: 3.0,
-                  color: Colors.black,
-                ),
-              ],
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: Offset(0, 3),
             ),
-          ),
-        ],
+          ],
+        ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: ColorFiltered(
+                colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.5),
+                  BlendMode.darken,
+                ),
+                child: Image.network(
+                  imageUrl,
+                  fit: BoxFit.cover,
+                  width: 200,
+                  height: 200,
+                ),
+              ),
+            ),
+            Text(
+              text,
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.purple[50],
+                shadows: [
+                  Shadow(
+                    offset: Offset(0.0, 1.0),
+                    blurRadius: 3.0,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
