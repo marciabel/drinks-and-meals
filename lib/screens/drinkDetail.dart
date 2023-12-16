ca43a83dde1830c2f2463d26df9ca12b462ed3cd
Pom94 <pamelavictoria-11@hotmail.com>
2023-12-16 03:44:34 -0300

    drinkDetail changes

--- lib/screens/drinkDetail.dart
+++ lib/screens/drinkDetail.dart
@@ -20,96 +20,97 @@ class DrinkDetail extends StatelessWidget {
       backgroundColor: Color.fromARGB(255, 35, 41, 70),
       appBar: CustomAppBar(title: ''),
       body: Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width * 0.8,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 200.0,
-                child: Image.network(
-                  imageUrl,
-                  fit: BoxFit.contain,
-                ),
-              ),
-              Text(
-                title,
-                style: TextStyle(
-                  color: Color.fromARGB(255, 238, 187, 195),
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 16.0),
-              Text(
-                'Ingredients',
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(200, 255, 255, 254),
-                  decoration: TextDecoration.underline,
-                ),
-              ),
-              Container(
-                //color: Colors.deepPurple[50],
-                child: Column(
-                  children: List.generate(
-                    ingredients.length,
-                    (index) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 4.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              ingredients[index]["name"],
-                              style: TextStyle(
-                                fontSize: 20.0,
-                                color: Color.fromARGB(200, 255, 255, 254),
-                                
-                                ),
-                              
-                            ),
-                            Spacer(),
-                            Text(
-                              ingredients[index]["measure"],
-                              style: TextStyle(fontSize: 20.0,
-                              color: Color.fromARGB(200, 255, 255, 254)
-                            ),
-                            ),
-                            SizedBox(width: 8.0),
-                          ],
-                        ),
-                      );
-                    },
+        child: SingleChildScrollView(
+          // Agrega SingleChildScrollView aquí
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 200.0,
+                  child: Image.network(
+                    imageUrl,
+                    fit: BoxFit.contain,
                   ),
                 ),
-              ),
-              SizedBox(height: 16.0),
-              Text(
-                'Instructions',
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(200, 255, 255, 254),
-                  decoration: TextDecoration.underline,
+                Text(
+                  title,
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 238, 187, 195),
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              SizedBox(height: 23.0),
-              Container(
-                color: Color.fromARGB(255, 236, 211, 215), // Cambia el color según tu preferencia
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    instructions,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 20.0,
+                SizedBox(height: 16.0),
+                Text(
+                  'Ingredients',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(200, 255, 255, 254),
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    children: List.generate(
+                      ingredients.length,
+                      (index) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 4.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                ingredients[index]["name"],
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  color: Color.fromARGB(200, 255, 255, 254),
+                                ),
+                              ),
+                              Spacer(),
+                              Text(
+                                ingredients[index]["measure"],
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  color: Color.fromARGB(200, 255, 255, 254),
+                                ),
+                              ),
+                              SizedBox(width: 8.0),
+                            ],
+                          ),
+                        );
+                      },
                     ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 16.0),
+                Text(
+                  'Instructions',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(200, 255, 255, 254),
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+                SizedBox(height: 23.0),
+                Container(
+                  color: Color.fromARGB(255, 236, 211, 215),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      instructions,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
